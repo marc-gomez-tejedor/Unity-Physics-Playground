660f1be5f5d4f200f72adc8c2194d6d6068cf70e
Marc <marcgomeztejedor@gmail.com>
2025-07-30 00:22:51 +0200

    fead: created static list accessible with all the objects that create a gravity field (AllGravityBodies)

---
+++ Assets/Scripts/Systems/Gravity/AllGravityBodies.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AllGravityBodies
+{
+    public static List<Rigidbody> MassBodies { get; private set; }
+}
