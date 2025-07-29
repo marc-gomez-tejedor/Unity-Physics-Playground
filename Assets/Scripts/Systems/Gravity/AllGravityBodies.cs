610c40fd24246dd4ea5324571e7d424a5f332ee0
Marc <marcgomeztejedor@gmail.com>
2025-07-30 01:02:38 +0200

    feat: added Newtons gravitational constant to AllGravityBodies.cs

--- Assets/Scripts/Systems/Gravity/AllGravityBodies.cs
+++ Assets/Scripts/Systems/Gravity/AllGravityBodies.cs
@@ -1,8 +1,8 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class AllGravityBodies
 {
     public static List<Rigidbody> MassBodies { get; private set; }
+    public static float NEWTONS_GRAVITATIONAL_CONSTANT { get; private set; } = 6f;
 }
