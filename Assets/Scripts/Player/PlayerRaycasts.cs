8afb8c9d80a2d1deaf46b242db324e359f64d386
Marc <marcgomeztejedor@gmail.com>
2025-09-04 14:10:04 +0200

    feat(player): first working player upright with spring system

--- Assets/Scripts/Player/PlayerRaycasts.cs
+++ Assets/Scripts/Player/PlayerRaycasts.cs
@@ -1,16 +1,32 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerRaycasts : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("General Parameters")]
+    private Vector3 DownDir;
+    [SerializeField] private float maxDistanceThreshold = 1f;
+    [SerializeField] private LayerMask groundLayerMask;
+    [SerializeField] private Transform raycastOrigin; 
 
-    // Update is called once per frame
-    void Update()
+    public (bool, RaycastHit) GetDownRaycastHit()
     {
-        
-    }
+        DownDir = -raycastOrigin.up;
+        DownDir = Vector3.down;
+        RaycastHit hit;
+        if (Physics.Raycast(origin: raycastOrigin.position, direction: DownDir, out hit,
+            maxDistance: maxDistanceThreshold, layerMask: groundLayerMask))
+        {
+            Debug.DrawRay(start: raycastOrigin.position, dir: DownDir * hit.distance, Color.green);
+            Debug.Log("Did Hit");
+            return (true, hit);
+        }
+        else
+        {
+            Debug.DrawRay(start: raycastOrigin.position, dir: DownDir * maxDistanceThreshold, Color.red);
+            Debug.Log("Did Hit");
+            return (false, new RaycastHit());
+        }
+    }    
 }
--- Assets/Scripts/Player/States/SimpleMoveState.cs
+++ Assets/Scripts/Player/States/SimpleMoveState.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class SimpleMoveState : PlayerState
 {
-    [Header("Parameters")]
+    [Header("General Parameters")]
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
 
+    [Header("spring parameters")]
+    [SerializeField] private float RideHeight = 0.5f; // offset for floating spring
+    [SerializeField] private float RideSpringStrength = 100f; // spring force
+    [SerializeField] private float RideSpringDamper = 15f; // spring damping force
+    
     public override void Act()
     {
         Vector2 inputDirection = Game.Input.MoveInput;
@@ -13,6 +18,46 @@ public class SimpleMoveState : PlayerState
         Debug.Log($"inpL: {mouseInput}");
         Debug.Log($"inpM: {inputDirection}");
         PlayerController.FPVrotation.Move(mouseInput);
+
+        /* -----------------------raycast----------------------------*/
+        (bool, RaycastHit) ray = PlayerController.raycasts.GetDownRaycastHit();
+        bool _rayDidHit = true;
+        RaycastHit _rayHit = ray.Item2;
+        Vector3 DownDir = -PlayerController.transform.up; //downwards raycast dir        
+        /* ----------------------------------------------------------*/
+
+        Rigidbody _RB = PlayerController._rigidbody;
+
+        if (_rayDidHit)
+        {
+            Vector3 vel = _RB.linearVelocity;
+            Vector3 rayDir = transform.TransformDirection(DownDir); //this should be =to forcefield
+            rayDir = Vector3.down; //placeholder
+            Vector3 otherVel = Vector3.zero;
+            Rigidbody hitBody = _rayHit.rigidbody;
+            if (hitBody != null)
+            {
+                otherVel = hitBody.linearVelocity;
+            }
+
+            float rayDirVel = Vector3.Dot(rayDir, vel);
+            float otherDirVel = Vector3.Dot(rayDir, otherVel);
+
+            float relVel = rayDirVel - otherDirVel;
+
+            float x = _rayHit.distance - RideHeight;
+
+            float springForce = (x * RideSpringStrength) - (relVel * RideSpringDamper);
+
+            //Debug.DrawLine(_RB.transform.position, _RB.transform.position + (rayDir * springForce), Color.yellow);
+
+            PlayerController._rigidbody.AddForce(rayDir * springForce);
+
+            if (hitBody != null)
+            {
+                hitBody.AddForceAtPosition(rayDir * -springForce, _rayHit.point);
+            }
+        }
     }
     public override void Jump()
     {
