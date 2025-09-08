b76cd080da88c209b0cc061bb2dd1964fd919e44
Marc <marcgomeztejedor@gmail.com>
2025-09-08 11:13:00 +0200

    feat(catlike): 2.5 desired velocity

--- Assets/Scripts/CatlikecodingTutorial/MovingSphere.cs
+++ Assets/Scripts/CatlikecodingTutorial/MovingSphere.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class MovingSphere : MonoBehaviour
 {
     [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f;
+    [SerializeField, Range(0f, 100f)]
     float maxSpeed = 10f;
     Vector3 velocity;
 
@@ -12,8 +14,10 @@ public class MovingSphere : MonoBehaviour
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        Vector3 acceleration = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
-        velocity += acceleration * Time.deltaTime;
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
         Vector3 displacement = velocity * Time.deltaTime;
         transform.localPosition += displacement;
     }
