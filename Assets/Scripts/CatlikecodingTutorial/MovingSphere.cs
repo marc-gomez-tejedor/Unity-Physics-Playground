172c12230ce9ece8e78a6f25ce377321e1d7996a
Marc <marcgomeztejedor@gmail.com>
2025-09-08 11:05:45 +0200

    feat(catklike): 2.4 Acceleration

--- Assets/Scripts/CatlikecodingTutorial/MovingSphere.cs
+++ Assets/Scripts/CatlikecodingTutorial/MovingSphere.cs
@@ -4,6 +4,7 @@ public class MovingSphere : MonoBehaviour
 {
     [SerializeField, Range(0f, 100f)]
     float maxSpeed = 10f;
+    Vector3 velocity;
 
     private void Update()
     {
@@ -11,7 +12,8 @@ public class MovingSphere : MonoBehaviour
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        Vector3 velocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        Vector3 acceleration = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        velocity += acceleration * Time.deltaTime;
         Vector3 displacement = velocity * Time.deltaTime;
         transform.localPosition += displacement;
     }
