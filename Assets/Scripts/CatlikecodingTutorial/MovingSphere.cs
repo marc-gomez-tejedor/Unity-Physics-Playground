c951e288e9ad9d11e3f0c2fd5b97e9caf211bf62
Marc <marcgomeztejedor@gmail.com>
2025-09-09 18:53:39 +0200

    feat(catlike/OrbitCamera): 3.2 Normalized Direction

--- Assets/Scripts/CatlikecodingTutorial/MovingSphere.cs
+++ Assets/Scripts/CatlikecodingTutorial/MovingSphere.cs
@@ -58,7 +58,13 @@ public class MovingSphere : MonoBehaviour
 
         if (playerInputSpace)
         {
-            desiredVelocity = playerInputSpace.TransformDirection(playerInput.x, 0f, playerInput.y) * maxSpeed;
+            Vector3 forward = playerInputSpace.forward;
+            forward.y = 0f;
+            forward.Normalize();
+            Vector3 right = playerInputSpace.right;
+            right.y = 0f;
+            right.Normalize();
+            desiredVelocity = (forward * playerInput.y + right * playerInput.x) * maxSpeed;
         }
         else
         {
