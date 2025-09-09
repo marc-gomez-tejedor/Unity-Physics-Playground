7b573f8e0302ae712a0221ae3a92e6402241dd95
Marc <marcgomeztejedor@gmail.com>
2025-09-09 16:25:38 +0200

    feat(catlike/OrbitCamera): 1.2 Maintaining Relative Position

--- Assets/Scripts/CatlikecodingTutorial/OrbitCamera.cs
+++ Assets/Scripts/CatlikecodingTutorial/OrbitCamera.cs
@@ -3,5 +3,16 @@ using UnityEngine;
 [RequireComponent (typeof(Camera))]
 public class OrbitCamera : MonoBehaviour
 {
-    
+    [SerializeField]
+    Transform focus = default;
+
+    [SerializeField, Range(1f, 20f)]
+    float distance = 5f;
+
+    private void LateUpdate()
+    {
+        Vector3 focusPoint = focus.position;
+        Vector3 lookDirection = transform.forward;
+        transform.localPosition = focusPoint - lookDirection * distance;
+    }
 }
