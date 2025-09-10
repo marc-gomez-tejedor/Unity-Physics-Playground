b72f67801caeba879f228a64fcb3dbd70e01208e
Marc <marcgomeztejedor@gmail.com>
2025-09-10 16:16:10 +0200

    feat(catlike/ComplexGravity): 1.2 Gravity Source List

--- Assets/Scripts/Systems/Gravity/CustomGravity.cs
+++ Assets/Scripts/Systems/Gravity/CustomGravity.cs
@@ -1,20 +1,35 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public static class CustomGravity
 {
+    static List<GravitySource> sources = new List<GravitySource>();
     public static Vector3 GetGravity(Vector3 position)
     {
-        return position.normalized * Physics.gravity.y;
+        Vector3 g = Vector3.zero;
+        for (int i = 0; i < sources.Count; i++)
+        {
+            g += sources[i].GetGravity(position);
+        }
+        return g;
     }
     public static Vector3 GetGravity(Vector3 position, out Vector3 upAxis)
     {
-        Vector3 up = position.normalized;
-        upAxis = Physics.gravity.y < 0f ? up : -up;
-        return upAxis * Physics.gravity.y;
+        Vector3 g = Vector3.zero;
+        for (int i = 0; i < sources.Count; i++)
+        {
+            g += sources[i].GetGravity(position);
+        }
+        upAxis = -g.normalized;
+        return g;
     }
     public static Vector3 GetUpAxis(Vector3 position)
     {
-        Vector3 up = position.normalized;
-        return Physics.gravity.y < 0f ? up : -up;
+        Vector3 g = Vector3.zero;
+        for (int i = 0; i < sources.Count; i++)
+        {
+            g += sources[i].GetGravity(position);
+        }
+        return -g.normalized;
     }
 }
