2e0df21d961937eb3dea2fb3dd486ea08b00f109
Marc <marcgomeztejedor@gmail.com>
2025-09-10 12:44:54 +0200

    feat(catlike/CustomGravity): 2.3 Pulling Toward the Origin

--- Assets/Scripts/Systems/Gravity/CustomGravity.cs
+++ Assets/Scripts/Systems/Gravity/CustomGravity.cs
@@ -4,15 +4,15 @@ public static class CustomGravity
 {
     public static Vector3 GetGravity(Vector3 position)
     {
-        return Physics.gravity;
+        return position.normalized * Physics.gravity.y;
     }
     public static Vector3 GetGravity(Vector3 position, out Vector3 upAxis)
     {
-        upAxis = -Physics.gravity.normalized;
-        return Physics.gravity;
+        upAxis = position.normalized;
+        return upAxis * Physics.gravity.y;
     }
     public static Vector3 GetUpAxis(Vector3 position)
     {
-        return -Physics.gravity.normalized;
+        return position.normalized;
     }
 }
