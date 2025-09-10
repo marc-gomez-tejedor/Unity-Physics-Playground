7fe1394d94f467ad4bb34498a0207ba9ef7e0800
Marc <marcgomeztejedor@gmail.com>
2025-09-10 13:18:19 +0200

    feat(catlike/CustomGravity): 3.2 Sleeping

--- Assets/Scripts/Systems/Gravity/CustomGravityRigidbody.cs
+++ Assets/Scripts/Systems/Gravity/CustomGravityRigidbody.cs
@@ -5,6 +5,8 @@ public class CustomGravityRigidbody : MonoBehaviour
 {
     Rigidbody body;
 
+    float floatDelay;
+
     void Awake()
     {
         body = GetComponent<Rigidbody>();
@@ -13,6 +15,23 @@ public class CustomGravityRigidbody : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (body.IsSleeping())
+        {
+            floatDelay = 0f;
+            return;
+        }
+        if (body.linearVelocity.sqrMagnitude < 0.0001f)
+        {
+            floatDelay += Time.deltaTime;
+            if (floatDelay > 1f)
+            {
+                return;
+            }
+        }
+        else
+        {
+            floatDelay = 0f;
+        }
         body.AddForce(CustomGravity.GetGravity(body.position), ForceMode.Acceleration);    
     }
 }
