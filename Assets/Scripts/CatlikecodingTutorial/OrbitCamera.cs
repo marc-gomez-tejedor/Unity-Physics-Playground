b4a3c487e7e1e519d17b72ebf8e621dfc7fe899b
Marc <marcgomeztejedor@gmail.com>
2025-09-10 18:04:03 +0200

    feat(catlike/ComplexGravity): 2.4 Camera Alignment Speed

--- Assets/Scripts/CatlikecodingTutorial/OrbitCamera.cs
+++ Assets/Scripts/CatlikecodingTutorial/OrbitCamera.cs
@@ -29,6 +29,9 @@ public class OrbitCamera : MonoBehaviour
     [SerializeField, Range(0f, 90f)]
     float alignSmoothRange = 45f;
 
+    [SerializeField, Min(0f)]
+    float upAlignmentSpeed = 360f;
+
     Camera regularCamera;
 
     Quaternion gravityAlignment = Quaternion.identity;
@@ -52,11 +55,7 @@ public class OrbitCamera : MonoBehaviour
 
     void LateUpdate()
     {
-        gravityAlignment =
-            Quaternion.FromToRotation
-            (
-                gravityAlignment * Vector3.up, CustomGravity.GetUpAxis(focusPoint)
-            ) * gravityAlignment;
+        UpdateGravityAlignment();
         UpdateFocusPoint();
         
         if (ManualRotation() || AutomaticRotation())
@@ -155,6 +154,23 @@ public class OrbitCamera : MonoBehaviour
         }
         orbitAngles.y = Mathf.MoveTowardsAngle(orbitAngles.y, headingAngle, rotationChange);
         return true;
+    }
+    void UpdateGravityAlignment()
+    {
+        Vector3 fromUp = gravityAlignment * Vector3.up;
+        Vector3 toUp = CustomGravity.GetUpAxis(focusPoint);
+        float dot = Mathf.Clamp(Vector3.Dot(fromUp, toUp), -1f, 1f);
+        float angle = Mathf.Acos(dot) * Mathf.Rad2Deg;
+        float maxAngle = upAlignmentSpeed * Time.deltaTime;
+        Quaternion newAlignment = Quaternion.FromToRotation(fromUp, toUp) * gravityAlignment;
+        if (angle <= maxAngle)
+        {
+            gravityAlignment = newAlignment;
+        }
+        else
+        {
+            gravityAlignment = Quaternion.SlerpUnclamped(gravityAlignment, newAlignment, maxAngle / angle);
+        }
     }
     void ConstrainAngles()
     {
