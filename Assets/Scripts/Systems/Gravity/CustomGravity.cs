fe22d599c8c3ffbb81372d4d97e7fd1246d0e0ff
Marc <marcgomeztejedor@gmail.com>
2025-09-10 12:52:35 +0200

    feat(catlike/CustomGravity): 2.4 Pushing Away

--- Assets/Scripts/Systems/Gravity/CustomGravity.cs
+++ Assets/Scripts/Systems/Gravity/CustomGravity.cs
@@ -8,11 +8,13 @@ public static class CustomGravity
     }
     public static Vector3 GetGravity(Vector3 position, out Vector3 upAxis)
     {
-        upAxis = position.normalized;
+        Vector3 up = position.normalized;
+        upAxis = Physics.gravity.y < 0f ? up : -up;
         return upAxis * Physics.gravity.y;
     }
     public static Vector3 GetUpAxis(Vector3 position)
     {
-        return position.normalized;
+        Vector3 up = position.normalized;
+        return Physics.gravity.y < 0f ? up : -up;
     }
 }
