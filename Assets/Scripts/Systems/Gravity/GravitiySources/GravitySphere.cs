1750bd8e29dd048b68aacf5d1ffbcef85338d644
Marc <marcgomeztejedor@gmail.com>
2025-09-11 19:14:19 +0200

    feat(catlike/ComplexGravity): 3.2 Applying Gravity

--- Assets/Scripts/Systems/Gravity/GravitiySources/GravitySphere.cs
+++ Assets/Scripts/Systems/Gravity/GravitiySources/GravitySphere.cs
@@ -8,13 +8,31 @@ public class GravitySphere : GravitySource
     [SerializeField, Min(0f)]
     float outerRadius = 10f, outerFalloffRadius = 15f;
 
+    float outerFalloffFactor;
+
     void OnValidate()
     {
         outerFalloffRadius = Mathf.Max(outerFalloffRadius, outerRadius);    
+        outerFalloffFactor = 1f / (outerFalloffRadius - outerRadius);
     }
     void Awake()
     {
         OnValidate();
+    }
+    public override Vector3 GetGravity(Vector3 position)
+    {
+        Vector3 vector = transform.position - position;
+        float distance = vector.magnitude;
+        if (distance > outerFalloffRadius)
+        {
+            return Vector3.zero;
+        }
+        float g = gravity / distance;
+        if (distance > outerRadius)
+        {
+            g *= 1f - (distance - outerRadius) * outerFalloffFactor;
+        }
+        return g * vector;
     }
     void OnDrawGizmos()
     {
