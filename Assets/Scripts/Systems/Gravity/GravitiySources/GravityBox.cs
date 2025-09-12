9dc41b8553301f2b6d990cfa2018a0a8da87126f
Marc <marcgomeztejedor@gmail.com>
2025-09-12 11:15:54 +0200

    fix(catlike/ComplexGravity): 4.2 Inner Distances

--- Assets/Scripts/Systems/Gravity/GravitiySources/GravityBox.cs
+++ Assets/Scripts/Systems/Gravity/GravitiySources/GravityBox.cs
@@ -6,7 +6,10 @@ public class GravityBox : GravitySource
     float gravity = 9.81f;
 
     [SerializeField]
-    Vector3 boundayDistance = Vector3.one;
+    Vector3 boundaryDistance = Vector3.one;
+
+    [SerializeField, Min(0f)]
+    float innerDistance = 0f, innerFalloffDistance = 0f;
 
     void Awake()
     {
@@ -14,12 +17,32 @@ public class GravityBox : GravitySource
     }
     void OnValidate()
     {
-        boundayDistance = Vector3.Max(boundayDistance, Vector3.zero);   
+        boundaryDistance = Vector3.Max(boundaryDistance, Vector3.zero);
+        float maxInner = Mathf.Min(Mathf.Min(boundaryDistance.x, boundaryDistance.y), boundaryDistance.z);
+        innerDistance = Mathf.Min(innerDistance, maxInner);
+        innerFalloffDistance = Mathf.Max(Mathf.Min(innerFalloffDistance, maxInner), innerDistance);
     }
     void OnDrawGizmos()
     {
         Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+        Vector3 size;
+        if (innerFalloffDistance > innerDistance)
+        {
+            Gizmos.color = Color.cyan;
+            size.x = 2f * (boundaryDistance.x - innerFalloffDistance);
+            size.y = 2f * (boundaryDistance.y - innerFalloffDistance);
+            size.z = 2f * (boundaryDistance.z - innerFalloffDistance);
+            Gizmos.DrawWireCube(Vector3.zero, size);
+        }
+        if (innerDistance > 0)
+        {
+            Gizmos.color = Color.yellow;
+            size.x = 2f * (boundaryDistance.x - innerDistance);
+            size.y = 2f * (boundaryDistance.y - innerDistance);
+            size.z = 2f * (boundaryDistance.z - innerDistance);
+            Gizmos.DrawWireCube(Vector3.zero, size);
+        }
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(Vector3.zero, 2f *  boundayDistance);
+        Gizmos.DrawWireCube(Vector3.zero, 2f *  boundaryDistance);
     }
 }
