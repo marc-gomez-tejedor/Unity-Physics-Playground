dc48e035a24da1ea4497645dc057a73bc553c29f
Marc <marcgomeztejedor@gmail.com>
2025-09-16 13:19:33 +0200

    feat(catlike/ReactiveEnvironment): 1.1 Zone Component

---
+++ Assets/Scripts/Systems/ReactiveEnvironment/AccelerationZone.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class AccelerationZone : MonoBehaviour
+{
+    [SerializeField, Min(0f)]
+    float speed = 10f;
+
+    void Accelerate(Rigidbody body)
+    {
+        Vector3 velocity = body.linearVelocity;
+        if (velocity.y >= speed)
+        {
+            return;
+        }
+
+        velocity.y = speed;
+        body.linearVelocity = velocity;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Rigidbody body = other.attachedRigidbody;
+        if (body)
+        {
+            Accelerate(body);
+        }
+    }
+
+}
