55743562c94d5a19d9d74cc56c8d46169c3e0c05
Marc <marcgomeztejedor@gmail.com>
2025-09-16 14:50:53 +0200

    feat(catlike/ReactiveEnvironment): 3.1 Automatic Slider

---
+++ Assets/Scripts/Systems/ReactiveEnvironment/AutomaticSlider.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AutomaticSlider : MonoBehaviour
+{
+    [SerializeField, Min(0.01f)]
+    float duration = 1f;
+
+    [SerializeField]
+    UnityEvent<float> onValueChanged = default;
+
+    float value;
+
+
+    void FixedUpdate()
+    {
+        value += Time.deltaTime / duration;
+        if (value >= 1f)
+        {
+            value = 1f;
+            enabled = false;
+        }
+    }
+}
