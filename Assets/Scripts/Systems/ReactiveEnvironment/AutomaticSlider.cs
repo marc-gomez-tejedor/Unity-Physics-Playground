26ab3535c1e63c9a57371f2a080dc4ce84799df1
Marc <marcgomeztejedor@gmail.com>
2025-09-16 17:20:41 +0200

    feat(catlike/ReactiveEnvironment): 3.3 Auto Reverse

--- Assets/Scripts/Systems/ReactiveEnvironment/AutomaticSlider.cs
+++ Assets/Scripts/Systems/ReactiveEnvironment/AutomaticSlider.cs
@@ -7,18 +7,52 @@ public class AutomaticSlider : MonoBehaviour
     float duration = 1f;
 
     [SerializeField]
+    bool autoReverse = false;
+
+    [SerializeField]
     UnityEvent<float> onValueChanged = default;
 
     float value;
 
+    bool reversed;
+
 
     void FixedUpdate()
     {
-        value += Time.deltaTime / duration;
-        if (value >= 1f)
+        float delta = Time.deltaTime / duration;
+        if (reversed)
         {
-            value = 1f;
-            enabled = false;
+            value -= delta;
+            if (value <= 0f)
+            {
+                if (autoReverse)
+                {
+                    value = Mathf.Min(1f, -value);
+                    reversed = false;
+                }
+                else
+                {
+                    value = 1f;
+                    enabled = false;
+                }
+            }
+        }
+        else
+        {
+            value += delta;
+            if (value >= 1f)
+            {
+                if (autoReverse)
+                {
+                    value = Mathf.Max(0f, 2f - value);
+                    reversed = true;
+                }
+                else
+                {
+                    value = 1f;
+                    enabled = false;
+                }
+            }
         }
         onValueChanged.Invoke(value);
     }
