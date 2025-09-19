bad582d08ce1cb2469894cbda3ed6bc9630c1f16
Marc <marcgomeztejedor@gmail.com>
2025-09-19 20:47:34 +0200

    feat(springDemo): 5 x 5 grid with different spring forces and ampings

--- Assets/Scripts/Player/States/SpringDemoState.cs
+++ Assets/Scripts/Player/States/SpringDemoState.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpringDemoState : PlayerState
 {
+    [SerializeField]
+    Rigidbody rb;
+
     [Header("Floating spring parameters")]
     [SerializeField] private float rideHeight = 0.5f; // offset for floating spring
     [SerializeField] private float rideSpringStrength = 100f; // spring force
@@ -18,9 +22,10 @@ public class SpringDemoState : PlayerState
     MeshRenderer meshRenderer;
     Color baseColor;
 
-
+    bool move = false, interact = false;
     private void Awake()
     {
+        rb = GetComponentInChildren<Rigidbody>();
         baseColor = meshRenderer.material.color;
     }
 
@@ -30,9 +35,26 @@ public class SpringDemoState : PlayerState
         float ratioDamper = 0.2f + 0.8f*(Mathf.Max(0f, (uprightSpringDamper - minDamper)/(maxDamper - minDamper)));
         Color temp = baseColor * ratioColor;
         meshRenderer.material.color = new Color(temp.r, temp.g, temp.b, ratioDamper);
+        if (Input.GetButtonDown("Interact"))
+        {
+            interact = true;
+        }
     }
     public override void Act()
     {
+        if (move)
+        {
+            rb.AddTorque(0f, 0f, -30f, ForceMode.Impulse);
+            move = false;
+        }
+        if (interact && !move)
+        {
+            move = true;
+            interact = false;
+            rb.angularVelocity = Vector3.zero;
+            rb.transform.rotation = Quaternion.identity;
+            return;
+        }
         PlayerController.Raycasts.UpdateRayCastDown();
         PlayerController.MovementBehaviour.UpdateFloatingSpringPosition(PlayerController, rideHeight, rideSpringStrength, rideSpringDamper);
         PlayerController.MovementBehaviour.UpdateUprightForce(PlayerController, uprightSpringStrength, uprightSpringDamper);
